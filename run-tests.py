d2d5dcb7c503acfc1b99ab71066f69375c3ecb94
Alexandro Sánchez Bach <alexaltea123@gmail.com>
2014-10-18 13:24:12 +0200

    Support for comparing output files

--- run-tests.py
+++ run-tests.py
@@ -5,8 +5,8 @@ import sys
 import subprocess
 
 # Constants
-OUTPUT_FILE   = 0 # The test generates a file called 'project.raw'
-OUTPUT_STDOUT = 1 # All output is obtained through stdout
+OUTPUT_STDOUT = 0 # All output is obtained through stdout
+OUTPUT_FILE   = 1 # The test generates a file called 'output.txt'
 
 COMPARE_BIN   = 0 # Output is validated byte per byte
 COMPARE_TEXT  = 1 # Output is validated as text, replacing CRLF with LF
@@ -14,7 +14,12 @@ COMPARE_SMART = 2 # Output is validated following rules specified below
 
 # List of tests and benchmarks
 autotests = (
-    ('tests/cpu/basic/basic', OUTPUT_STDOUT, COMPARE_TEXT),
+    ('tests/cpu/basic',                   OUTPUT_STDOUT,  COMPARE_TEXT),
+    ('tests/cpu/ppu_integer_arithmetic',  OUTPUT_FILE,    COMPARE_TEXT),
+    ('tests/cpu/ppu_integer_compare',     OUTPUT_FILE,    COMPARE_TEXT),
+    ('tests/cpu/ppu_integer_logical',     OUTPUT_FILE,    COMPARE_TEXT),
+    ('tests/cpu/ppu_integer_rotate',      OUTPUT_FILE,    COMPARE_TEXT),
+    ('tests/cpu/ppu_integer_shift',       OUTPUT_FILE,    COMPARE_TEXT),
 )
 
 # Main
@@ -24,9 +29,11 @@ def runTests(emulator, baseDir):
     # Run tests
     for test in autotests:
         # Parameters
-        project = test[0][:test[0].rfind('/')]
-        elfPath = test[0] + '.ppu.self'
-        expectedPath = test[0] + '.expected'
+        projectFolder = test[0]
+        projectName = projectFolder[projectFolder.rfind('/')+1:]
+        elfPath = projectFolder + '/' + projectName + '.ppu.self'
+        expectedPath = projectFolder + '/' + projectName + '.expected'
+        outputPath = projectFolder + '/output.txt'
         outputMethod = test[1]
         compareMethod = test[2]
 
@@ -37,16 +44,23 @@ def runTests(emulator, baseDir):
         # Get output
         if outputMethod == OUTPUT_STDOUT:
             result = subprocess.check_output(cmd, shell=True)
+        if outputMethod == OUTPUT_FILE:
+            subprocess.check_output(cmd, shell=True);
+            result = open(outputPath, 'rb').read()
 
         # Compare output
         if compareMethod == COMPARE_TEXT:
             result = result.replace('\r\n', '\n')
             if result == expected.read():
-                print " - Success:", project
+                print " - Success:", projectFolder
             else:
-                print " - Error:", project
+                print " - Error:", projectFolder
                 print result
                 errors = True
+
+        # Delete output file if necessary
+        if outputMethod == OUTPUT_FILE:
+            os.remove(outputPath)
 
     # Return errors
     if errors:
